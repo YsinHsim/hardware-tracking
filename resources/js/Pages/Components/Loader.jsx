626aab339cf11f36d3edf89fff479fac0169a556
Muhammad Khairulazhar Bin Kahar <khairulazhar2004@gmail.com>
2025-03-08 16:34:50 +0800

    add looader for the page

---
+++ resources/js/Pages/Components/Loader.jsx
@@ -0,0 +1,10 @@
+const Loader = () => {
+    return (
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 backdrop-blur-sm z-50">
+        <span className="loading loading-bars loading-xl text-primary"></span>
+      </div>
+    );
+  };
+  
+  export default Loader;
+  
